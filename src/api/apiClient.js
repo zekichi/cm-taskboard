0cf76ad2debfb571f15af6b010a8955e06a7bd28
Ezequiel <equintana779@gmail.com>
2026-04-29 21:41:32 -0300

    api 2

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+// Cliente API propio para comunicar el frontend con el backend
 export const api = axios.create({
-  baseURL: "http://localhost:4000/api", // ← después lo ajustamos si cambia
+  baseURL: "http://localhost:4000/api", // Cambiar si usás otro puerto
   headers: {
     "Content-Type": "application/json",
   },
